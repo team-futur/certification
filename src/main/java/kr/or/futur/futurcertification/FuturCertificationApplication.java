a48adec3f24561c0d02f6a2dcb316423c84af36c
jinsung <201519011@yiu.ac.kr>
2023-10-31 21:04:35 +0900

    DB 비활성화 및 Eureka Client 설정

--- src/main/java/kr/or/futur/futurcertification/FuturCertificationApplication.java
+++ src/main/java/kr/or/futur/futurcertification/FuturCertificationApplication.java
@@ -2,7 +2,9 @@ package kr.or.futur.futurcertification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FuturCertificationApplication {
 
