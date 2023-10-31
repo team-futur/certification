4001cf4c8fe9d91501c06bd8477e935e3605bbc0
jinsung <201519011@yiu.ac.kr>
2023-10-31 20:48:51 +0900

    Init

---
+++ src/main/java/kr/or/futur/futurcertification/FuturCertificationApplication.java
@@ -0,0 +1,13 @@
+package kr.or.futur.futurcertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturCertificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuturCertificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/or/futur/futurcertification/FuturCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.futur.futurcertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturCertificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
