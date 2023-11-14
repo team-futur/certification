38312152a2f62d4cd788ccc8faca248f17d9925b
jinsung <201519011@yiu.ac.kr>
2023-11-14 20:27:39 +0900

    Test 코드 Profile 보도록 수정

--- src/test/java/kr/or/futur/futurcertification/FuturCertificationApplicationTests.java
+++ src/test/java/kr/or/futur/futurcertification/FuturCertificationApplicationTests.java
@@ -2,7 +2,9 @@ package kr.or.futur.futurcertification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class FuturCertificationApplicationTests {
 
--- src/test/java/kr/or/futur/futurcertification/controller/CertificationControllerTest.java
+++ src/test/java/kr/or/futur/futurcertification/controller/CertificationControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("local")
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 class CertificationControllerTest {
